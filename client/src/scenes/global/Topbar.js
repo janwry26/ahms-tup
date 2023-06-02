2c97b00fda4940a4f4ee47c6df1675311a882858
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-06-02 11:46:33 +0800

    remove search bar from dashboard

--- client/src/scenes/global/Topbar.js
+++ client/src/scenes/global/Topbar.js
@@ -4,7 +4,6 @@ import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
 
 const Topbar = () => {
   const theme = useTheme();
@@ -55,10 +54,6 @@ const Topbar = () => {
         backgroundColor={colors.primary[400]}
         borderRadius="3px"
       >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
-        </IconButton>
       </Box>
 
       {/* ICONS */}
