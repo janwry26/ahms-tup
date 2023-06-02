0ae92f7d980762a3f44355074de8868120efdebc
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-06-02 13:08:29 +0800

    added line chart for the analytics of inventory

--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -1,116 +1,142 @@
-import { ResponsiveLine } from "@nivo/line";
-import { useTheme } from "@mui/material";
-import { tokens } from "../theme";
-import { mockLineData as data } from "../data/mockData";
-import "../styles/loader.css"
-const LineChart = ({ isCustomLineColors = false, isDashboard = false }) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  return (
-    <ResponsiveLine
-      data={data}
-      theme={{
-        axis: {
-          domain: {
-            line: {
-              stroke: colors.grey[100],
-            },
-          },
-          legend: {
-            text: {
-              fill: colors.grey[100],
-            },
-          },
-          ticks: {
-            line: {
-              stroke: colors.grey[100],
-              strokeWidth: 1,
-            },
-            text: {
-              fill: colors.grey[100],
-            },
-          },
-        },
-        legends: {
-          text: {
-            fill: colors.grey[100],
-          },
-        },
-        tooltip: {
-          container: {
-            color: colors.primary[500],
-          },
-        },
-      }}
-      colors={isDashboard ? { datum: "color" } : { scheme: "nivo" }} // added
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: "point" }}
-      yScale={{
-        type: "linear",
-        min: "auto",
-        max: "auto",
-        stacked: true,
-        reverse: false,
-      }}
-      yFormat=" >-.2f"
-      curve="catmullRom"
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        orient: "bottom",
-        tickSize: 0,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: isDashboard ? undefined : "transportation", // added
-        legendOffset: 36,
-        legendPosition: "middle",
-      }}
-      axisLeft={{
-        orient: "left",
-        tickValues: 5, // added
-        tickSize: 3,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: isDashboard ? undefined : "count", // added
-        legendOffset: -40,
-        legendPosition: "middle",
-      }}
-      enableGridX={false}
-      enableGridY={false}
-      pointSize={8}
-      pointColor={{ theme: "background" }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: "serieColor" }}
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
+import { useEffect, useState, useRef } from 'react';
+import http from '../utils/http';
+import Chart from 'chart.js/auto';
+import moment from 'moment';
+import '../styles/charts.css';
+
+const LineChart = () => {
+  const [products, setProducts] = useState([]);
+  const chartRef = useRef(null);
+
+  const getProducts = () => {
+    http.get('/inventory/view')
+      .then((res) => {
+        const products = res.data.map((product, key) => ({
+          id: key + 1,
+          _id: product._id,
+          expDate: product.expDate,
+          itemDescription: product.itemDescription,
+          itemName: product.itemName,
+          itemType: product.itemType,
+          quantity: product.quantity,
+        }));
+        setProducts(products);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  useEffect(() => {
+    if (chartRef.current !== null) {
+      chartRef.current.destroy();
+    }
+
+    const chartElement = document.getElementById('lineChart');
+    const chartData = {
+      labels: products.map((product) => product.itemName),
+      datasets: [
         {
-          anchor: "bottom-right",
-          direction: "column",
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: "left-to-right",
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: "circle",
-          symbolBorderColor: "rgba(0, 0, 0, .5)",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemBackground: "rgba(0, 0, 0, .03)",
-                itemOpacity: 1,
+          label: 'Quantity',
+          data: products.map((product) => product.quantity),
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 2,
+          lineTension: 0,
+        },
+      ],
+    };
+
+    const maxQuantityProduct = products.reduce((prevProduct, currentProduct) => {
+      return prevProduct.quantity > currentProduct.quantity ? prevProduct : currentProduct;
+    }, {});
+
+    const today = moment();
+    const soonToExpireProduct = products.find((product) => {
+      const expirationDate = moment(product.expDate);
+      return expirationDate.diff(today, 'days') <= 30;
+    });
+
+    chartRef.current = new Chart(chartElement, {
+      type: 'line',
+      data: chartData,
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+            grid: {
+              display: true,
+              color: '#5cc0af',
+              borderColor: '#5cc0af',
+              lineWidth: 2,
+            },
+            ticks: {
+              stepSize: 10,
+              font: {
+                size: 18,
+                color: '#ffffff',
               },
             },
-          ],
+          },
+          x: {
+            grid: {
+              display: true,
+              color: '#5cc0af',
+              borderColor: '#5cc0af',
+              lineWidth: 2,
+            },
+            ticks: {
+              font: {
+                size: 18,
+                color: '#ffffff',
+              },
+            },
+          },
         },
-      ]}
-    />
-  );
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              font: {
+                size: 18,
+                color: '#ffffff',
+              },
+              generateLabels: function (chart) {
+                const labels = Chart.defaults.plugins.legend.labels.generateLabels(chart);
+                const maxQuantityLabel = {
+                  text: `Most Quantity: ${maxQuantityProduct.itemName}`,
+                  fillStyle: 'rgba(75, 192, 192, 1)',
+                  font: {
+                    size: 22,
+                    color: '#ffffff',
+                  },
+                };
+                labels.push(maxQuantityLabel);
+                if (soonToExpireProduct) {
+                  const expirationDate = moment(soonToExpireProduct.expDate);
+                  const expirationLabel = {
+                    text: `Soon to Expire: ${soonToExpireProduct.itemName} (${expirationDate.format('MM/DD/YYYY')})`,
+                    fillStyle: 'red',
+                    font: {
+                      size: 16,
+                      color: '#ffffff',
+                    },
+                  };
+                  labels.push(expirationLabel);
+                }
+                return labels;
+              },
+            },
+          },
+        },
+      },
+    });
+  }, [products]);
+
+  return <canvas id="lineChart" width="400" height="200"></canvas>;
 };
 
 export default LineChart;
