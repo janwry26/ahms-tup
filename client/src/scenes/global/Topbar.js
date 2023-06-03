95218f8cd90aea6ce4199f82589fe5409c9bdc33
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-06-03 14:55:57 +0800

    added notification

--- client/src/scenes/global/Topbar.js
+++ client/src/scenes/global/Topbar.js
@@ -1,9 +1,10 @@
-import { Box, IconButton, useTheme, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, TextField } from "@mui/material";
-import { useContext, useState } from "react";
+import { Box, IconButton, useTheme, Menu, MenuItem, Badge, Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, TextField } from "@mui/material";
+import { useContext, useState,useEffect } from "react";
 import { ColorModeContext, tokens } from "../../theme";
-import InputBase from "@mui/material/InputBase";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import "../../styles/topbar.css"
+import http from "../../utils/http";
 
 const Topbar = () => {
   const theme = useTheme();
@@ -11,7 +12,31 @@ const Topbar = () => {
   const colorMode = useContext(ColorModeContext);
   const [anchorEl, setAnchorEl] = useState(null);
   const [isProfileDialogOpen, setIsProfileDialogOpen] = useState(false);
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [notificationCount, setNotificationCount] = useState(1); // Update the notification count based on your logic
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    http.get('/inventory/view')
+        .then((res) => {
+          const products = res.data.map((product, key) => ({
+            id: key+1,
+            _id: product._id,
+            expDate: product.expDate,
+            itemDescription: product.itemDescription,
+            itemName: product.itemName,
+            itemType: product.itemType,
+            quantity: product.quantity,
+          }));
+          setProducts(products);
+        })
+        .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getProducts();
+  },[])
 
   const userProfile = {
     username: "janwryMock",
@@ -26,6 +51,17 @@ const Topbar = () => {
   };
 
   const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleNotificationClick = (event) => {
+    setIsNotificationOpen(true);
+    setAnchorEl(event.currentTarget);
+    setNotificationCount(0); // Reset the notification count when clicked
+  };
+
+  const handleNotificationClose = () => {
+    setIsNotificationOpen(false);
     setAnchorEl(null);
   };
 
@@ -58,8 +94,10 @@ const Topbar = () => {
 
       {/* ICONS */}
       <Box display="flex">
-        <IconButton>
-          <NotificationsOutlinedIcon />
+        <IconButton onClick={handleNotificationClick}>
+          <Badge badgeContent={notificationCount} color="error">
+            <NotificationsOutlinedIcon />
+          </Badge>
         </IconButton>
         <IconButton onClick={handleMenuOpen}>
           <PersonOutlinedIcon />
@@ -72,10 +110,22 @@ const Topbar = () => {
           <MenuItem onClick={handleViewProfile}>View Profile</MenuItem>
           <MenuItem onClick={handleLogout}>Logout</MenuItem>
           {isLoading && (
-         <div className="loader-overlayLogout">
-         <div className="loaderLogout"></div>
-       </div>
-        )}
+            <div className="loader-overlayLogout">
+              <div className="loaderLogout"></div>
+            </div>
+          )}
+        </Menu>
+        <Menu
+          anchorEl={anchorEl}
+          open={isNotificationOpen}
+          onClose={handleNotificationClose}
+        >
+           <h5 className="notification-title">Notifications</h5>
+          <MenuItem>New item added on inventory</MenuItem>
+          <MenuItem>New added animal record</MenuItem>
+          <MenuItem>New added mortality report</MenuItem>
+          <MenuItem>New added observation report</MenuItem>
+          <MenuItem>New added medical history record</MenuItem>
         </Menu>
       </Box>
 
