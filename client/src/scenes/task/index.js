0ac0bce666e9e35cd6e752d3249e9643d7b81506
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-06-04 18:47:11 +0800

    change ui of task

--- client/src/scenes/task/index.js
+++ client/src/scenes/task/index.js
@@ -1,118 +1,178 @@
+
+import { Form, Button } from "react-bootstrap";
+import { Box, Dialog, DialogTitle, DialogContent, DialogActions, TextField,InputLabel,Select } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { FaPlus, FaArchive, FaEdit } from "react-icons/fa";
+import HowToRegIcon from '@mui/icons-material/HowToReg';
 import Header from "../../components/Header";
-import { Button, Form } from "react-bootstrap";
 import Swal from "sweetalert2";
-import { Box,TextField } from "@mui/material";
-import { Select, MenuItem,InputLabel } from "@mui/material";
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 import { useState,useEffect } from "react";
 import "../../styles/loader.css"
 import http from "../../utils/http";
+import { formatDate } from "../../utils/formatDate";
 
 
-function TaskList() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState("");
-  const [selectedStaffId, setSelectedStaffId] = useState("");
-  const [taskID, setTaskID] = useState("");
-  const [taskDueDate, setTaskDueDate] = useState("");
-  const [taskStatus, setTaskStatus] = useState("Not Complete"); // Set initial task status
+const ObservationReport = () => {
+  const [reports, setReports] = useState([]);
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
+  const [editReport, setEditReport] = useState(null);
+  const [animalList, setAnimalList] = useState([]);
+  const [staffList, setStaffList] = useState([]);
 
-  const getTask = () => {
-    http.get('/task/view')
+   const getObservationReport = () => {
+    http.get('/observation-report/view')
+    .then((res) => {
+      const reportPromises = res.data.map((report, key) => {
+        const animalRequest = http.get(`/animal/view/${report.animalID}`);
+        const staffRequest = http.get(`/user/view/${report.staffID}`);
+
+        return Promise.all([animalRequest, staffRequest])
+          .then(([animalRes, staffRes]) => {
+            const animalName = animalRes.data.animalName;
+            const staffName = `${staffRes.data.lastName}, ${staffRes.data.firstName}`;
+
+            return {
+               id: key+1,
+              _id: report._id,
+              animalID: report.animalID,
+              staffID: report.staffID,
+              animalName: animalName,
+              staffName: staffName,
+              reportDescription: report.reportDescription,
+              dateReported: formatDate(report.dateReported),
+            };
+          });
+      });
+
+      Promise.all(reportPromises)
+        .then((reports) => {
+          setReports(reports);
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+  }
+
+  const getAnimals = () => {
+    
+    http.get('/animal/view')
         .then((res) => {
-          const tasks = res.data.map((task, key) => ({
-            id: key+1,
-            _id: task._id,
-            taskID: task.taskID,
-            staffID: task.staffID,
-            taskName: task.taskName,
-            dueDate: task.dueDate,
-            taskStatus: task.taskStatus,
-          }));
-          setTasks(tasks);
+          setAnimalList(res.data);
         })
         .catch((err) => console.log(err));
   }
 
+  const getStaffs = () => {
+    
+    http.get('/user/view')
+        .then((res) => {
+          setStaffList(res.data);
+        })
+        .catch((err) => console.log(err));
+  }
+  
   useEffect(() => {
-    getTask();
+    getObservationReport();
+    getAnimals();
+    getStaffs();
   },[])
 
-  const handleSubmit = (event) => {
+  const handleAddReport = (event) => {
     event.preventDefault();
-    if (!newTask || selectedStaffId === 0) return;
-    const task = {
-      taskID: taskID,
-      userID: selectedStaffId,
-      taskName: newTask,
-      taskDueDate: taskDueDate,
-      taskStatus: taskStatus,
+    http
+      .post('/observation-report/add', {
+        animalID: event.target.animalID.value,
+        staffID: event.target.staffID.value,
+        reportDescription: event.target.reportDescription.value,
+        dateReported: event.target.dateReported.value,
+      })
+      .then((res) => {
+        console.log(res);
+        Swal.fire({
+          title: 'Success',
+          text: 'Product added to inventory',
+          icon: 'success',
+          timer: 700, // Show the alert for 2 seconds
+          showConfirmButton: false
+        });
+        getObservationReport(); // Refresh the products list
+      })
+      .catch((err) => console.log(err));
+    event.target.reset();
+  };
+
+  const handleDeleteReport = (_id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this product!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        http
+          .delete(`/observation-report/delete/${_id}`)
+          .then((res) => {
+            console.log(res);
+            Swal.fire('Deleted!', 'Your product has been deleted.', 'success');
+            getObservationReport(); // Refresh the products list
+          })
+          .catch((err) => console.log(err));
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your product is safe :)', 'error');
+      }
+    });
+  };
+
+  const handleEditReport = (params, event) => {
+    const { id, field, props } = params;
+    const { value } = event.target;
+    const newReports = reports.map((report) => {
+      if (report.id === id) {
+        return { ...report, [field]: value };
+      }
+      return report;
+    });
+    setReports(newReports);
+  };
+
+  const handleEditDialogOpen = (report) => {
+    setEditReport(report);
+    setEditDialogOpen(true);
+  };
+
+  const handleEditDialogClose = () => {
+    setEditDialogOpen(false);
+  };
+
+  const handleEditDialogSave = () => {
+    const editedReport = {
+      animalName: document.getElementById("editAnimalName").value, // Update animalID instead of animalName
+      staffName: document.getElementById("editStaffName").value,
+      reportDescription: document.getElementById("editReportDescription").value,
+      dateReported: document.getElementById("editDateReported").value,
     };
-    setTasks([...tasks, task]);
-    setNewTask("");
-    setSelectedStaffId("");
-    setTaskID("");
-    setTaskDueDate("");
-    setTaskStatus("Not Complete"); // Reset task status to "Not Complete" after submission
-  };
-
-  const handleDelete = (id) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        setTasks(tasks.filter((task) => task.taskID !== id));
-        Swal.fire("Deleted!", "Your task has been deleted.", "success");
-      }
-    });
-  };
-
-  const handleUpdate = (id, newText) => {
-    Swal.fire({
-      title: "Update task",
-      input: "text",
-      text: "You can update your task by typing here!",
-      icon: "info",
-      inputValue: newText,
-      showCancelButton: true,
-      confirmButtonText: "Save",
-      cancelButtonText: "Cancel",
-      inputValidator: (value) => {
-        if (!value) {
-          return "You need to enter a new task name!";
-        }
-      },
-    }).then((result) => {
-      if (result.isConfirmed) {
-        setTasks(
-          tasks.map((task) =>
-            task.taskID === id ? { ...task, taskName: result.value } : task
-          )
+  
+    http
+      .put(`/observation-report/edit/${editReport._id}`, editedReport)
+      .then((res) => {
+        const updatedReports = reports.map((report) =>
+          report._id === editReport._id ? { ...report, ...editedReport } : report
         );
-        Swal.fire("Updated!", "Your task has been updated.", "success");
-      }
-    });
+        setReports(updatedReports);
+        setEditDialogOpen(false);
+        Swal.fire('Success', 'Product updated successfully!', 'success').then(()=>window.location.reload());
+      })
+      .catch((err) => console.log(err));
   };
-
-  const handleComplete = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.taskID === id
-          ? {
-              ...task,
-              taskStatus: "Complete",
-              taskAccomplishDate: new Date().toLocaleString(),
-            }
-          : task
-      )
-    );
-  };
-
+  
+  
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   const [isLoading, setIsLoading] = useState(true); 
   useEffect(() => {
     // Simulate loading delay
@@ -128,141 +188,216 @@ function TaskList() {
     <div className="loader1"></div>
   </div> // Render the loader while loading
   }
-
   return (
-    <Box p="20px" width="80%" margin="0 auto" paddingTop="50px" className="reload-animation">
-      <div>
-        <Header
-          title="TASK"
-          subtitle="Managing task for staff"
-          fontSize="36px"
-          mt="20px"
-        />
-        <Form onSubmit={handleSubmit}>
-           <Box mt={2}>
-           <InputLabel >Task Name</InputLabel>
+    <Box m="20px" width="80%" margin="0 auto" className="reload-animation">
+      <Header
+        title="Task Management"
+        subtitle="Manage Task Pages"
+        fontSize="36px"
+        mt="20px"
+      />
+  <Form onSubmit={handleAddReport}>
+  <Box marginBottom="10px">
+        <InputLabel >Task Name</InputLabel>
           <TextField
-            fullWidth
-            variant="filled"
-            placeholder="Enter Task Name..."
-            value={newTask}
-            onChange={(event) => setNewTask(event.target.value)}
-          />
-        </Box>
-        <Box mt={2}>
-        <InputLabel >Staff ID</InputLabel>
-
-          <TextField
-            fullWidth
-            variant="filled"
-            placeholder="Enter Staff ID..."
-            value={selectedStaffId}
-            onChange={(event) => setSelectedStaffId(event.target.value)}
-            type="number"
-          />
-        </Box>
-        <Box mt={2}>
-        <InputLabel >Task ID</InputLabel>
-          <TextField
-            fullWidth
-            variant="filled"
-            placeholder="Enter Task ID"
-            value={taskID}
-            onChange={(event) => setTaskID(event.target.value)}
-            type="number"
-          />
-        </Box>
-        <Box mt={2}>
-        <InputLabel >Due Date</InputLabel>
-          <TextField
-            fullWidth
-            variant="filled"
-            placeholder="Select Due Date"
-            type="date"
-            value={taskDueDate}
-            onChange={(event) => setTaskDueDate(event.target.value)}
-          />
-        </Box>
-        <Box mt={2}>
-        <InputLabel >Task Status</InputLabel>
-
-          <Select
-            fullWidth
-            variant="filled"
-            placeholder="Select task status"
-            value={taskStatus}
-            onChange={(event) => setTaskStatus(event.target.value)}
-          >
-            <MenuItem value="Not Complete">Not Complete</MenuItem>
-            <MenuItem value="Complete">Complete</MenuItem>
-          </Select>
-        </Box>
-        <Button
-              className="btnDashBoard"
-              type="submit"
-              color="success"
-              variant="success"
-            >
-              Create
-            </Button>
-        </Form>
-
-        <h2 className="mt-4">TASK CREATED BELOW:</h2>
-
-        {tasks.length > 0 && (
-          <div className="mt-3">
-            {tasks.map((task) => (
-              <div
-                key={task.taskID}
-                className="d-flex justify-content-between align-items-center p-3 mb-3 shadow-sm"
-              >
-                <div>
-                  <h4 style={{color:"rgb(119,213,203)"}}>{task.taskName}</h4>
-                  <p>Assigned to Staff ID {task.staffID}</p>
-                  <p>Status: {task.taskStatus}</p>
-                  <p>Due Date {task.dueDate} </p>
-                  {task.taskAccomplishDate && (
-                    <p>Completed on: {task.taskAccomplishDate}</p>
-                  )}
-                </div>
-                <div>
-                  {task.taskStatus !== "Complete" && (
-                    <Button
-                      variant="success"
-                      onClick={() => handleComplete(task.taskID)}
-                    >
-                      Complete
-                    </Button>
-                  )}
-                  <Button
-                    variant="warning"
-                    className="mx-2"
-                    onClick={() =>
-                      handleUpdate(task.taskID, task.taskName)
-                    }
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    variant="danger"
-                    onClick={() => handleDelete(task.taskID)}
-                  >
-                    Delete
-                  </Button>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-
-        {tasks.length === 0 && (
-          <div className="mt-3">
-            <p>No tasks have been added yet. Please add a task.</p>
-          </div>
-        )}
-      </div>
+              placeholder="Input Task Name..."
+              name="reportDescription"
+              variant="filled"
+              fullWidth
+              required
+            />
     </Box>
-  );
-}
 
-export default TaskList;
+
+    <Box marginBottom="10px">
+    <InputLabel>Staff</InputLabel>
+          <Select
+            name="staffID"
+            native
+            fullWidth
+            required
+            variant="filled"
+          >
+            <option value="" >Select a Staff</option>
+            {staffList.map((val) => {
+                return (
+                  <option value={val.staffId} key={val.staffId}>{val.lastName + ', ' + val.firstName}</option>
+                )
+            })}          
+          </Select>
+    </Box>
+
+    <Box marginBottom="10px">
+        <InputLabel >Task Description</InputLabel>
+          <TextField
+              placeholder="Input Task Description..."
+              name="reportDescription"
+              variant="filled"
+              fullWidth
+              required
+            />
+    </Box>
+
+    <Box marginBottom="10px">
+        <InputLabel >Task Due Date</InputLabel>
+          <TextField
+              name="dateReported"
+              variant="filled"
+              fullWidth
+              required
+              type="date" 
+            />
+    </Box>
+
+    <div className="d-grid gap-2" style={{marginTop:"-20px", marginBottom: "20px"}}>
+      <Button className="btnDashBoard"  type="submit"  >
+        <FaPlus /> Add Task
+      </Button>
+    </div>
+  </Form>
+
+  <Box
+    m="40px 0 0 0"
+    height="75vh"
+    margin= "0 auto"
+    sx={{
+      // Styling for the DataGrid
+      "& .MuiDataGrid-root": {
+        border: "none",
+      },
+      "& .MuiDataGrid-cell": {
+        borderBottom: "none",
+      },
+      "& .MuiDataGrid-columnHeaders": {
+        backgroundColor: colors.greenAccent[700],
+        borderBottom: "none",
+      },
+      "& .MuiDataGrid-virtualScroller": {
+        backgroundColor: colors.primary[400],
+      },
+      "& .MuiDataGrid-footerContainer": {
+        borderTop: "none",
+        backgroundColor: colors.greenAccent[700],
+      },
+      "& .MuiCheckbox-root": {
+        color: `${colors.greenAccent[200]} !important`,
+      },
+      "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+        color: `${colors.grey[100]} !important`,
+      },
+    }}
+  >
+    <DataGrid
+      rows={reports}
+      columns={[ 
+        { field: "taskName",headerName: "Task Name", flex: 1 },
+
+        { field: "staffName", headerName: "Staff Name", flex: 1 },  
+        { field: "taskDescription", headerName: "Task Description", flex: 1 },
+        { field: "dueDate", headerName: "Due Date", flex: 1 },  
+         { 
+          field: "actions",
+           headerName: "Actions", 
+            sortable: false, 
+             filterable: false, 
+              renderCell: (params) => 
+              (<div> 
+               <Button  className="btn btn-sm mx-1" variant="primary" onClick={() => handleDeleteReport(params.row._id)}>
+                 <FaArchive />
+                  </Button> 
+                <Button className="mx-1" variant="warning" size="sm" onClick={() => handleEditDialogOpen(params.row)}>
+                <FaEdit />
+              </Button>
+              <Button  variant="success" size="sm" onClick={() => handleEditDialogOpen(params.row)}>
+                <HowToRegIcon />
+              </Button>
+            </div>
+          ),
+          flex: 0.5,
+        },
+      ]}
+      components={{ Toolbar: GridToolbar }}
+    />
+  </Box>
+
+  {/* Edit Dialog */}
+  <Dialog open={editDialogOpen} onClose={handleEditDialogClose}>
+    <DialogTitle>Edit Report</DialogTitle>
+    <DialogContent>
+      <Form onSubmit={handleEditReport}>
+  
+        <Box marginBottom="10px">
+        <InputLabel >Animal</InputLabel>
+        <Select
+            id="editAnimalName"
+            defaultValue={editReport ? editReport.animalID : ""}
+            native
+            fullWidth
+            required
+            variant="filled"
+          >
+            <option value="" >Select an Animal</option>
+            {animalList.map((val) => {
+                return (
+                  <option value={val.animalID} key={val.animalID}>{val.animalName}</option>
+                )
+            })}          
+          </Select>
+    </Box>
+    <Form.Group className="mb-3" controlId="editReportDescription">
+         
+        </Form.Group>
+         <Box marginBottom="10px">
+    <InputLabel>Staff</InputLabel>
+          <Select
+            id="editStaffName"
+            native
+            fullWidth
+            required
+            defaultValue={editReport ? editReport.staffID : ""}
+            variant="filled"
+          >
+            <option value="" >Select a Staff</option>
+            {staffList.map((val) => {
+                return (
+                  <option value={val.staffId} key={val.staffId}>{val.lastName + ', ' + val.firstName}</option>
+                )
+            })}          
+          </Select>
+    </Box>
+
+        <Form.Group className="mb-3" controlId="editReportDescription">
+          <Form.Label>Report Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Enter report description"
+            defaultValue={editReport ? editReport.reportDescription : ""}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="editDateReported">
+          <Form.Label>Date Reported</Form.Label>
+          <Form.Control
+            type="date"
+            defaultValue={editReport ? editReport.dateReported : ""}
+            required
+          />
+        </Form.Group>
+      </Form>
+    </DialogContent>
+    <DialogActions>
+      <Button variant="warning" onClick={handleEditDialogClose}>
+        Cancel
+      </Button>
+      <Button variant="success"  color="danger"onClick={handleEditDialogSave} type="submit">
+        Save
+      </Button>
+    </DialogActions>
+  </Dialog>
+</Box>
+);
+};
+
+export default ObservationReport;
