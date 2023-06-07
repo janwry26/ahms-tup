ea4d3a53096eca1badbc9e29712fd53987b47c8f
Janwry <janwry.jrv@gmail.com>
2023-06-07 23:22:45 +0800

    Update baseURL

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001/api"
-    // baseURL: "https://dst-voting-system.vercel.app/api"
+    // baseURL: "http://localhost:3001/api"
+    baseURL: "https://ahms-tup.vercel.app/api"
 })
 
 export default instance;
