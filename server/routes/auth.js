26365c04c28e29b68161cb32ad055f7694eb1be8
Janwry Dela Cruz <112063309+janwry26@users.noreply.github.com>
2023-06-08 01:41:42 +0800

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/User");
 const Admin = require("../models/Admin");
 const _ = require("lodash");
 const jwt = require("jsonwebtoken");
