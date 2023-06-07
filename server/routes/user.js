b3746082a0860c4fca595fc289eb87630d2725a6
Janwry Dela Cruz <112063309+janwry26@users.noreply.github.com>
2023-06-08 01:31:34 +0800

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/User");
 const getNextCounterValue = require("./counterUtils");
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
