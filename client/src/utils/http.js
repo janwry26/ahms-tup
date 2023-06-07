2983c3fde82b53e4523259419c87f290a7c1b8fa
Janwry Dela Cruz <112063309+janwry26@users.noreply.github.com>
2023-06-08 01:30:40 +0800

    Update http.js

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: "http://localhost:3001/api"
-    baseURL: "https://ahms-tup.vercel.app/api"
+    baseURL: "https://ahms.onrender.com/api"
 })
 
 export default instance;
