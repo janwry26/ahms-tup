13261f119aba159cc85c07931d51616a04792de3
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-06-09 21:55:24 +0800

    updated base url

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: "http://localhost:3001/api"
-    baseURL: "https://api-animal-healthcare.onrender.com"
+    baseURL: "https://api-animal-healthcare.onrender.com/api/"
 })
 
 export default instance;
