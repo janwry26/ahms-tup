f853d7187060690df9d82319c4362f541bfeccf2
Janwry Dela Cruz <112063309+janwry26@users.noreply.github.com>
2023-06-11 13:52:31 +0800

    Update http.js

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001/api"
-    // baseURL: "https://api-animal-healthcare.onrender.com/api/"
+    // baseURL: "http://localhost:3001/api"
+    baseURL: "https://api-animal-healthcare.onrender.com/api/"
 })
 
 export default instance;
