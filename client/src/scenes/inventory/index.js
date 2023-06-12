cdce3024b4e6d27b6bc6d50db02bf51153cde794
janwry26 <janwry.jrv@gmail.com>
2023-06-13 01:03:27 +0800

    added height and scroll on the input fields

--- client/src/scenes/inventory/index.js
+++ client/src/scenes/inventory/index.js
@@ -127,11 +127,14 @@ const Inventory = () => {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
+    height: 500,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    overflowY: 'scroll',
   };
+  
 
   const handleEditDialogOpen = (product) => {
     setEditProduct(product);
