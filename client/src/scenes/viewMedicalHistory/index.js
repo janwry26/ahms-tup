bdcb0050508def067ea78eea0ad99da1cbfa8fff
janwry26 <janwry.jrv@gmail.com>
2023-06-13 01:32:28 +0800

    added height and scroll

--- client/src/scenes/viewMedicalHistory/index.js
+++ client/src/scenes/viewMedicalHistory/index.js
@@ -28,12 +28,13 @@ const ViewMedicalHistory = () => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 500,
+    width: 400,
+    height: 500,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  
+    overflowY: 'scroll',
   };
   
 
