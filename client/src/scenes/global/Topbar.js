718c56b76c5a07ceec98373b1c3c57563082a36e
Janwry Dela Cruz <janwry.jrv@gmail.com>
2023-06-20 01:25:33 +0800

    added notification

--- client/src/scenes/global/Topbar.js
+++ client/src/scenes/global/Topbar.js
@@ -2,7 +2,8 @@ import { Box, IconButton, useTheme, Menu, MenuItem, Badge, Dialog, DialogTitle, 
 import { useContext, useState, useEffect } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import ClearIcon from "@mui/icons-material/Clear";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import { format } from "date-fns";
 import "../../styles/topbar.css";
 import http from "../../utils/http";
 import jwtDecode from 'jwt-decode';
@@ -15,6 +16,8 @@ const Topbar = () => {
   const [currentUser, setCurrentUser] = useState("");
   const [isAdmin, setIsAdmin] = useState(false);
   const [anchorElProfile, setAnchorElProfile] = useState(null);
+  const [anchorElNotifications, setAnchorElNotifications] = useState(null);
+  const [products, setProducts] = useState([]);
 
   const getCurrentUser = () => {
     const token = localStorage.getItem('token');
@@ -28,13 +31,14 @@ const Topbar = () => {
         .then((res) => {
           setCurrentUser(res.data);
         })
+        .catch((err) => console.log(err));
     }
   };
 
   useEffect(() => {
     getCurrentUser();
+    getProducts();
   }, []);
-
 
   const handleLogout = () => {
     setIsLoading(true);
@@ -52,7 +56,13 @@ const Topbar = () => {
     setAnchorElProfile(null);
   };
 
- 
+  const handleNotificationsOpen = (event) => {
+    setAnchorElNotifications(event.currentTarget);
+  };
+
+  const handleNotificationsClose = () => {
+    setAnchorElNotifications(null);
+  };
 
   const handleViewProfile = () => {
     setIsProfileDialogOpen(true);
@@ -63,6 +73,42 @@ const Topbar = () => {
     setIsProfileDialogOpen(false);
   };
 
+  const getProducts = () => {
+    http.get('/inventory/view')
+      .then((res) => {
+        const products = res.data.map((product, key) => ({
+          id: key + 1,
+          _id: product._id,
+          category: product.category,
+          unitOfMeasure: product.unitOfMeasure,
+          manufacturer: product.manufacturer,
+          supplier: product.supplier,
+          dateAdded: format(new Date(product.dateAdded), "MMMM d, yyyy"),
+          expDate: format(new Date(product.expDate), "MMMM d, yyyy"),
+          itemDescription: product.itemDescription,
+          itemName: product.itemName,
+          itemType: product.itemType,
+          quantity: product.quantity,
+        }));
+
+        const sortedProductsByQuantity = [...products].sort((a, b) => a.quantity - b.quantity);
+        const sortedProductsByExpirationDate = [...products].sort((a, b) => new Date(a.expDate) - new Date(b.expDate));
+
+        const leastQuantityProducts = sortedProductsByQuantity.slice(0, 5);
+        const soonestExpirationProducts = sortedProductsByExpirationDate.slice(0, 5);
+
+        const notificationItems = leastQuantityProducts.map((product) => ({
+          id: product.id,
+          itemName: product.itemName,
+          quantity: product.quantity,
+          expDate: product.expDate,
+        }));
+
+        setProducts(notificationItems.concat(soonestExpirationProducts));
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
       {/* SEARCH BAR */}
@@ -70,10 +116,55 @@ const Topbar = () => {
 
       {/* ICONS */}
       <Box display="flex">
+        <IconButton onClick={handleNotificationsOpen}>
+          <Badge badgeContent={products.length} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <Menu
+          anchorEl={anchorElNotifications}
+          open={Boolean(anchorElNotifications)}
+          onClose={handleNotificationsClose}
+          className="notification-menu"
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          getContentAnchorEl={null}
+        >
+          {products.map((product) => (
+            <MenuItem key={product.id}>
+              <Box display="flex" flexDirection="column">
+                <Typography variant="body1" color="textPrimary">{product.itemName}</Typography>
+                <Typography variant="body2" color="textSecondary">{`Quantity: ${product.quantity}`}</Typography>
+                <Typography variant="body2" color="textSecondary">{`Expiration Date: ${product.expDate}`}</Typography>
+              </Box>
+            </MenuItem>
+          ))}
+        </Menu>
+
         <IconButton onClick={handleMenuOpen}>
           <PersonOutlinedIcon />
         </IconButton>
-        <Menu anchorEl={anchorElProfile} open={Boolean(anchorElProfile)} onClose={handleMenuClose}>
+        <Menu
+          anchorEl={anchorElProfile}
+          open={Boolean(anchorElProfile)}
+          onClose={handleMenuClose}
+          className="profile-menu"
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          getContentAnchorEl={null}
+        >
           <MenuItem onClick={handleViewProfile}>View Profile</MenuItem>
           <MenuItem onClick={handleLogout}>Logout</MenuItem>
           {isLoading && (
@@ -82,7 +173,6 @@ const Topbar = () => {
             </div>
           )}
         </Menu>
-       
       </Box>
 
       {/* PROFILE DIALOG */}
